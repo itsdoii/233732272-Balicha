163ca788ecdb7bd419d3b13898098b63e4b68d76
itsdoii <dhaaaiiilan@gmail.com>
2024-12-04 06:09:45 +0800

    Added Javascript

---
+++ FCC JavaScript/Pyramid Generator/script.js
@@ -0,0 +1,31 @@
+function generatePyramid() {
+  const character = "!";
+  const count = 10;
+  const rows = [];
+  const seed = Math.random();
+  let inverted = seed > 0.5;
+
+  function padRow(rowNumber, rowCount) {
+    return (
+      " ".repeat(rowCount - rowNumber) +
+      character.repeat(2 * rowNumber - 1) +
+      " ".repeat(rowCount - rowNumber)
+    );
+  }
+
+  for (let i = 1; i <= count; i++) {
+    if (inverted) {
+      rows.unshift(padRow(i, count));
+    } else {
+      rows.push(padRow(i, count));
+    }
+  }
+
+  let result = "";
+
+  for (const row of rows) {
+    result = result + "\n" + row + "<br>";
+  }
+
+  document.getElementById("pyramid").innerHTML = result;
+}
